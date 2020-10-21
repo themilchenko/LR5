74e77853b3b6bdad9067a78f071e430480a7ca18
Ivan <70635146+themilchenko@users.noreply.github.com>
2020-10-21 14:28:59 +0300

    Update LR5.cpp

--- LR5.cpp
+++ LR5.cpp
@@ -1,46 +1,82 @@
-﻿#include <iostream>
+/*
+*   LR_5 // VARIANT_19
+*/
+
+#include <iostream>
 #include <string>
 #include <unordered_map>
 #include <algorithm>
 #include <map>
+#include <vector>
+#include <functional>
 
 void print(std::pair<int, std::string> n) 
 {
 	std::cout << n.first << ") = " << n.second << "; " << std::endl;
 }
 
+void print_vec(int t)
+{
+	std::cout << t << " ";
+}
+
 int main()
 {
 	std::cout << "Task1: " << std::endl;
-	std::unordered_multimap<int, std::string> keys = { {1, "key1"}, {2, "key2"}, {3, "key3"}, {4, "key1"}, {5, "key2"}, {6, "key3"} };
+	std::unordered_multimap<int, std::string> keys = { {-1, "earth"}, {2, "wind"}, {1, "fire"}, {-4, "low"}, {-1, "high"}, {6, "word"} };
 	std::for_each(keys.begin(), keys.end(), print); 
 	std::cout << std::endl;
 
-	std::cout << "Task 1.1: " << std::endl;
-	keys.insert(std::make_pair<int, std::string>(4, "new1"));
-	keys.insert(std::make_pair<int, std::string>(4, "new2"));
-	std::for_each(keys.begin(), keys.end(), print);
-	std::cout << std::endl;
-
-	std::cout << "Task 1.2: " << std::endl;
+	std::cout << "Task 1.1: " << std::endl; // delete elements with keys "1" and "2"
 	for (auto t = keys.begin(); t != keys.end(); t++)
 	{
-		if (t->first == 2)
-			keys.erase(t);
-		break;
+		if ((t->first == 1) || (t->first == 2))
+			t = keys.erase(t);
+		else
+			++t;
 	}
 	std::for_each(keys.begin(), keys.end(), print);
 	std::cout << std::endl;
 
-	std::cout << "Task 1.3: " << std::endl;
+	std::cout << "Task 1.2: " << std::endl; // insert 3 new values with key "3"
+	keys.insert(std::make_pair<int, std::string>(3, "new1"));
+	keys.insert(std::make_pair<int, std::string>(3, "new2"));
+	keys.insert(std::make_pair<int, std::string>(3, "new3"));
+	std::for_each(keys.begin(), keys.end(), print);
+	std::cout << std::endl;
+
+	std::cout << "Task 1.3: " << std::endl; // copy elements from "unordered_multimap" to "map"
 	std::map <int, std::string> cards;
 	for (auto i : keys)
 		cards.insert(std::make_pair(i.first, i.second));
 	std::for_each(cards.begin(), cards.end(), print); 
 	std::cout << std::endl;
 
-	std::cout << "Task 2.1: " << std::endl;
+	std::cout << "Task 2: " << std::endl;
+	std::cout << "Task 2.1: " << std::endl; // count number of elements, which have keys in the range from -2 to 6
+	int negative_counter = std::count_if(cards.begin(), cards.end(), [](int i) {return ((i > -2) && (i < 6)); });
+	std::cout << "Number of this elements: " << negative_counter << std::endl;
 
+	std::cout << "Task 2.2: " << std::endl; // transform all keys in "vector" by std::transform
+	std::vector<int> keys_vector;
+	std::transform(cards.begin(), cards.end(), std::back_inserter(keys_vector), [](const std::pair<int, std::string> i) {return i.first; });
+	std::for_each(keys_vector.begin(), keys_vector.end(), print_vec);
+	std::cout << std::endl;
+
+	std::cout << "Task 3: " << std::endl;
+	std::cout << "Task 3.1: " << std::endl; // find element equal "3"
+	auto finder_of_3 = std::find(keys_vector.begin(), keys_vector.end(), 3);
+	if (finder_of_3 == keys_vector.end())
+		std::cout << "There is no one '3' in this vector" << std::endl;
+	else
+		std::cout << finder_of_3 - keys_vector.begin() << std::endl;
+
+	std::cout << "Task 3.2: " << std::endl; // repleace negative elements on "0"
+	std::replace_if(keys_vector.begin(), keys_vector.end(), std::bind(std::less<int>(), std::placeholders::_1, 3), 0);
+	std::for_each(keys_vector.begin(), keys_vector.end(), print_vec);
+	std::cout << std::endl;
+
+	std::cout << "Task 3.3: " << std::endl;
 
 	return 0;
 }
